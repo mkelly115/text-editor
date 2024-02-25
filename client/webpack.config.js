67f279736ed20f716b76c0dd56f6409590e11fb3
michael <mikek222@gmail.com>
2024-02-25 15:30:28 +0300

    this might work, honestly zero clue, needs testing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,41 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Your App Name',
+        short_name: 'App',
+        description: 'Your app description',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          // Specify icons for your app
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/service-worker.js', // Path to your service worker file
+        swDest: 'service-worker.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
